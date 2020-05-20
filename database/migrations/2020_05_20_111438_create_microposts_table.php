4aff54066614b09c9ccc2d5adfee03a60a2909f4
ree-km <reina.harada@artemis-tech.jp>
2020-05-20 04:02:27 +0000

    post

---
+++ database/migrations/2020_05_20_111438_create_microposts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMicropostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('microposts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->string('content');
+            $table->timestamps();
+
+            // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('microposts');
+    }
+}
